5ef61c501f2a5f25a8bb921709ef8eaaa7e1d4b6
Marek Bis <xbisma00@stud.fit.vut.cz>
2022-11-21 18:44:48 +0100

    update video.c

--- testing.c
+++ testing.c
@@ -10,45 +10,52 @@ struct cluster_t {
     int capacity;
     struct obj_t *obj;
 };
-/*
- Inicializace shluku 'c'. Alokuje pamet pro kapacitu objektu (cap).
- Ukazatel NULL u pole objektu znamena kapacitu 0.
-*/
-void init_cluster(struct cluster_t *c, int cap){
+int load_clusters(char *filename, struct cluster_t **arr)
+{
+    assert(arr != NULL);
+
     // TODO
-}
-
-void vytvorit(){
-
-}
-int main () {
-    struct cluster_t *clusters;
-    clusters[0].size=10;
-    clusters[0].capacity=100;
-    clusters[0].obj->id=1;
-    clusters[0].obj->x=10;
-    clusters[0].obj->y=20;
-
-printf("%d %d %d %f %f",clusters[0].size,clusters[0].capacity,
-        clusters[0].obj->id,clusters[0].obj->x,clusters[0].obj->y);
-
-    struct obj_t obj1;
-    struct cluster_t clust1;
     FILE * filePointer;
-    char count[20];
-   filePointer = fopen ("objekty.txt", "r");
-    for (int radek = 0;radek<=20;radek++){
+    int count = 20; 
+    filePointer = fopen ("objekty.txt", "r");
+    for (int radek = 0;radek<=count;radek++){
         if (radek !=0){
-        fscanf(filePointer,"%d %f %f", &obj1.id,&obj1.x,&obj1.y);
+        fscanf(filePointer,"%d %f %f",);
         printf("Cluster %d: %d[%.1f, %.1f]\n",radek,obj1.id,obj1.x,obj1.y);
         }else{
             fscanf(filePointer,"%s",count);
             printf("%s\n",count);
         }
     }
+    fclose(filePointer);
+}
+
+void print_cluster(struct cluster_t *c)
+{
+    // TUTO FUNKCI NEMENTE
+    for (int i = 0; i < c->size; i++)
+    {
+        if (i) putchar(' ');
+        printf("%d[%g,%g]", c->obj[i].id, c->obj[i].x, c->obj[i].y);
+    }
+    putchar('\n');
+}
+
+void print_clusters(struct cluster_t *carr, int narr){
+    printf("Clusters:\n");
+    for (int i = 0; i < narr; i++)
+    {
+        printf("cluster %d: ", i);
+        print_cluster(&carr[i]);
+    }
+}
+
+int main () {
+    int n;
+    struct cluster_t *clusters;    
 
 
-   fclose(filePointer);
+   
    
    return 0;
 }
